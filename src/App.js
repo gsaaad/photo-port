c173a5def61f01867f382a9a0583a9b70c556095
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-11 00:11:29 -0400

    added About component with some navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import About from "./components/About";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <div>
+      <Nav></Nav>
+      <main>
         <About></About>
-      </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+function Nav() {
+  const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    {
+      name: "portraits",
+      description: "Portraits of people in my life",
+    },
+    {
+      name: "food",
+      description: "Delicious Delicaies",
+    },
+    {
+      name: "landscape",
+      description: "Fields, Farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+
+  function categorySelected(name) {
+    console.log(`Hello!, You clicked on ${name}`);
+  }
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+        <nav>
+          <ul className="flex-row">
+            <li className="mx-2">
+              <a href="#about">About me</a>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+
+            {/* this is mapping and creating an list item for each category picture */}
+            {categories.map((category) => (
+              <li className="mx-1" key={category.name}>
+                <span onClick={() => categorySelected(category.name)}>
+                  {category.name}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </h2>
+    </header>
+  );
+}
+export default Nav;
