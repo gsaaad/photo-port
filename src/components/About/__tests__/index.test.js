709857988ab02fe46920dc3f30d78aca98915c25
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-11 00:47:08 -0400

    added tests to check if About component renders correctly using asFragment to snapshot

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+
+afterEach(cleanup);
+
+describe("About component", () => {
+  //render about Test
+  //   first test
+  it("rendersonce", () => {
+    render(<About />);
+  });
+  //   first test rewritten
+  test("renderstwice", () => {
+    render(<About />);
+  });
+  // second test
+  describe("matches snapshot DOM node structure", () => {
+    //asFragment returns a snapshot of DOM
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+// simulated DOM with render
+// clean up remove components from DOM to prevent memory leak
