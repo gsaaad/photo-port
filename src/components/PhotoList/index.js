5b6a5155d3892bd020982e8c1123c1ed5934d34f
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-17 01:15:13 -0400

    added Modal to pictures

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import Modal from "../Modal";
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
   const [photos] = useState([
     {
       name: "Grocery aisle",
@@ -120,19 +124,25 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div>
+      {isModalOpen && (
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
-          <div>
-            <img
-              src={require(`../../assets/small/${category}/${i}.jpg`)}
-              alt={image.name}
-              className="img-thumbnail mx-1"
-              key={image.name}
-            />
-            <h1 data-testid="h1Title">{image.name}</h1>
-          </div>
+          <img
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
+            key={image.name}
+          />
         ))}
       </div>
     </div>
